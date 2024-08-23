52fae5a87e4832555ddf1e8017ffddafa6832451
syednasrulla007 <syednasrulla085@gmail.com>
2024-08-23 21:48:33 +0530

    first commit

---
+++ src/Data/SuggestedProduct.js
@@ -0,0 +1,18 @@
+export const SuggestedProducts = [
+    {
+      id: 1,
+      thumbnail: require("../Assets/images/Nike-Air-Max-97-Air-Force.png"),
+      title: "Nike Air Max",
+    },
+    {
+      id: 2,
+      thumbnail: require("../Assets/images/Nike-Air-Max-Sneakers-red-white.png"),
+      title: "Sneakers",
+    },
+    {
+      id: 3,
+      thumbnail: require("../Assets/images/Nike-Shoe-Running.png"),
+      title: "Running Shoe",
+    },
+  ];
+  
